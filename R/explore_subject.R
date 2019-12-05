52835997060056a26671e4d7429cbdd2966978f7
Shawn <shawn_williams@brown.edu>
2019-12-05 15:18:45 -0500

    renames original function to explore and made explore_nature a wrapper function that either calls explore if no page range is requested (will default to pg 1 if no input or to a single requested page, if a range is given, will map explore across the page range and combine into single tibble

--- R/explore_subject.R
+++ R/explore_subject.R
@@ -1,8 +1,25 @@
-explore_subject <- function(subject = NULL, ...){
+explore<- function(subject = NULL, ...){
   if (is.null(subject)) (stop('A subject query is required, see help for examples'))
-  
   url.constructor(subject, ...) %>%
-    nature.scrape()
+  nature.scrape()
 }
+
+
+multi.explore <- function(cpage, ...){
+  explore(..., pages = cpage)
+}
+
+
+
+explore_nature <- function(subject = NULL, page.range = NULL, ...){
+  if (is.null(page.range)){
+    explore(subject = subject)
+  } 
+  else {
+    map(page.range, ~ multi.explore(.x, subject = subject)) %>%
+      merge.tables
+    }
+}
+
 
 
