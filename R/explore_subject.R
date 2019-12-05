eb8be69b6d323428d5dea8c99b1a417e2c82cd73
Shawn <shawn_williams@brown.edu>
2019-12-05 14:34:46 -0500

    Function to search papers by subject area

---
+++ R/explore_subject.R
@@ -0,0 +1,8 @@
+explore_subject <- function(subject = NULL, ...){
+  if (is.null(subject)) (stop('A subject query is required, see help for examples'))
+  
+  url.constructor(subject, ...) %>%
+    nature.scrape()
+}
+
+
