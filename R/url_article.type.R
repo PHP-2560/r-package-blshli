ddba56c176514fc2ddd01f598fad517733d5c5f1
Shawn <shawn_williams@brown.edu>
2019-12-05 14:38:55 -0500

    added within url constructor

--- R/url_article.type.R
+++
@@ -1,6 +0,0 @@
-# url_article.type
-# function to generate string for url search query from string of given article types 
-url_article.type <- function(article.types)
-  article.types %>%
-  str_replace_all('\\s','%2C') %>%
-  return()
