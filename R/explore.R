8c20afc50d7a3c51d0060273c1878c090eb8ae90
Shawn <shawn_williams@brown.edu>
2019-12-07 15:47:40 -0500

    nm

--- R/explore.R
+++ R/explore.R
@@ -1,5 +1,4 @@
 explore<- function(subject = NULL, ...){
-  require(dplyr)
   if (is.null(subject)) (stop('A subject query is required, see help for examples'))
   url.constructor(subject, ...) %>%
     nature.scrape()
