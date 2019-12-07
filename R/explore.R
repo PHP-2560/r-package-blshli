017a44c506450fb2521bf09c2a4018736a165b2d
Shawn <shawn_williams@brown.edu>
2019-12-07 15:40:10 -0500

    testing require statement

--- R/explore.R
+++ R/explore.R
@@ -1,4 +1,5 @@
 explore<- function(subject = NULL, ...){
+  require(dplyr)
   if (is.null(subject)) (stop('A subject query is required, see help for examples'))
   url.constructor(subject, ...) %>%
     nature.scrape()
