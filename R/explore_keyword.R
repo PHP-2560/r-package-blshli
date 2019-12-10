58fa6f88a17c14b9686379ce2f018d21fb9c6041
Shawn <shawn_williams@brown.edu>
2019-12-10 16:06:49 -0500

    fixed bug

--- R/explore_keyword.R
+++ R/explore_keyword.R
@@ -1,4 +1,5 @@
-explore_keyword <- function(subject = NULL, page.range = NULL, ...){
+explore_keyword <- function(keyword = NULL, page.range = NULL, ...){
+  subject = keyword
   if (length(subject) > 1 ) (subject <- str_flatten(subject,collapse = ' '))
   ### quick note for manual, allowing both range and singe page to be provided at the same time but ranges will take precedence 
   if (is.null(page.range)){
@@ -10,4 +11,4 @@ explore_keyword <- function(subject = NULL, page.range = NULL, ...){
     }
 }
 
-
+explore()
