83b6c317c0071865445d372d83fd3fc201079e3f
Shawn <shawn_williams@brown.edu>
2019-12-10 13:25:49 -0500

    typo fix

--- R/url_constructor.R
+++ R/url_constructor.R
@@ -12,7 +12,7 @@ subject <- str_replace_all( subject, '\\s', replacement = '%20')
 base.url <- paste0(base.url, subject)
 
 
-if ((!is.null(publication.range)) & (!is.null(l.year))) stop('provide only one publication range or lower year modifiers ')
+if ((!is.null(publication.range)) & (!is.null(l.year))) stop('provide only one publication range or lower year modifier')
 
 # extending the url
 if (! is.null(article.types)){
