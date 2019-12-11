86c9ec1a921e9dd3477545e065e9eb330b638332
Shawn <shawn_williams@brown.edu>
2019-12-11 14:53:08 -0500

    updates to manual and pages renamed page

--- R/explore_keyword.R
+++ R/explore_keyword.R
@@ -1,18 +1,18 @@
-explore_keyword <- function(keyword = NULL, article.types = NULL, sort.method = NULL,
+explore_keyword <- function(keyword, article.types = NULL, sort.method = NULL,
                             publication.range = NULL,  l.year = NULL, 
-                            u.year = NULL, pages = NULL, page.range = NULL){
+                            u.year = NULL, page = 1, page.range = NULL){
   subject = keyword
   if (length(subject) > 1 ) (subject <- str_flatten(subject,collapse = ' '))
   ### quick note for manual, allowing both range and singe page to be provided at the same time but ranges will take precedence 
   if (is.null(page.range)){
     explore(subject, article.types , sort.method ,
             publication.range ,  l.year , 
-            u.year , pages , page.range )
+            u.year , page , page.range )
   } 
   else{
     map(page.range, ~ multi.explore(.x, subject,article.types , sort.method ,
                                     publication.range ,  l.year , 
-                                    u.year , pages )) %>%
+                                    u.year , page )) %>%
       merge.tables
     }
 }
