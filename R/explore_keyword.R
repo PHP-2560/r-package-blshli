7b19050efe58064cdcd9f956b9bc8829f97754c6
Shawn <shawn_williams@brown.edu>
2019-12-11 15:15:22 -0500

    typos

--- R/explore_keyword.R
+++ R/explore_keyword.R
@@ -1,18 +1,18 @@
 explore_keyword <- function(keyword, article.types = NULL, sort.method = NULL,
                             publication.range = NULL,  l.year = NULL, 
-                            u.year = NULL, page = 1, page.range = NULL){
+                            u.year = NULL, page.range = NULL){
   subject = keyword
   if (length(subject) > 1 ) (subject <- str_flatten(subject,collapse = ' '))
   ### quick note for manual, allowing both range and singe page to be provided at the same time but ranges will take precedence 
   if (is.null(page.range)){
     explore(subject, article.types , sort.method ,
             publication.range ,  l.year , 
-            u.year , page , page.range )
+            u.year, pages = 1 )
   } 
   else{
     map(page.range, ~ multi.explore(.x, subject,article.types , sort.method ,
                                     publication.range ,  l.year , 
-                                    u.year , page )) %>%
+                                    u.year )) %>%
       merge.tables
     }
 }
