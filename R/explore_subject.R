281a71556891feec3f9d713f95f584b316ded76c
Shawn <shawn_williams@brown.edu>
2019-12-11 15:16:44 -0500

    typo

--- R/explore_subject.R
+++ R/explore_subject.R
@@ -1,6 +1,6 @@
 explore_subject <- function(subject = NULL, article.types = NULL, sort.method = NULL,
                             publication.range = NULL,  l.year = NULL, 
-                            u.year = NULL, pages = 1,page.range = NULL){
+                            u.year = NULL, pages = 1, page.range = NULL){
   if (length(subject) > 1 ) stop('Only queries for 1 subject is supported')
   if ( ! (subject %in% list.topics())) stop(paste(subject, 'is not supported, use list.topics() to find supported subjects'))
   subject <- str_replace_all(subject, '\\s', '-') %>%
@@ -10,7 +10,7 @@ explore_subject <- function(subject = NULL, article.types = NULL, sort.method = 
   if (is.null(page.range)){
     explore(subject,article.types , sort.method ,
             publication.range ,  l.year , 
-            u.year , pages )
+            u.year, pages )
   } 
   else{
     map(page.range, ~ multi.explore(.x,subject,article.types , sort.method ,
