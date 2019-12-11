5c20c73ddb3bd76b459ecbcf3d8db02283df3e67
Shawn <shawn_williams@brown.edu>
2019-12-11 14:00:49 -0500

    fixed library

--- Testing package.R
+++ Testing package.R
@@ -2,7 +2,7 @@
 library(devtools)
 
 library(dplyr)
-install_github("PHP-2560/r-package-blshli" )
+install_github("PHP-2560/r-package-blshli", force = T, build_vignettes = T )
 library(DiscoverNature)
 
 paper.results <- explore_keyword('stem cells')
@@ -11,5 +11,5 @@ paper.results %>%
 
 explore_paper(.title = 'Tppp3+', res = paper.results)
 
-vingnette('DiscoverNature')
+vignette('DiscoverNature')
 
